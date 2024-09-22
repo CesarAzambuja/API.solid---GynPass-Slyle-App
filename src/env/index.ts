72843aca65f06ae8030c6ca6c78697e93b1f1aed
cesarazambuja <dev.azambuja@outlook.com>
2024-09-22 11:38:23 -0300

    configured Eslint in project

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,11 +8,10 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
+if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
 
-  throw new Error ('Invalid environment variables')
-
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
